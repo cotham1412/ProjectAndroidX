debf411632a81922e933f657587cfdd03daf7a51
20211tt4780@mail.tdc.edu.vn <20211tt4780@mail.tdc.edu.vn>
2022-05-15 18:10:00 +0700

    upload 1

--- ProjectAndroidX/app/src/main/java/com/example/projectandroidx/ui/home1/Home1Fragment.java
+++ ProjectAndroidX/app/src/main/java/com/example/projectandroidx/ui/home1/Home1Fragment.java
@@ -1,33 +1,49 @@
 package com.example.projectandroidx.ui.home1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.projectandroidx.MainActivity;
 import com.example.projectandroidx.R;
+import com.example.projectandroidx.databinding.FragmentHome1Binding;
 import com.example.projectandroidx.databinding.FragmentHomeBinding;
+import com.example.projectandroidx.ui.home.HomeFragment;
 import com.example.projectandroidx.ui.home.HomeViewModel;
 
-public class HomeFragment extends Fragment {
+public class Home1Fragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentHome1Binding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        com.example.projectandroidx.ui.home.HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentHome1Binding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final Button button = binding.button4;
+
+        button.setText("ksksksk");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TAG", "onCreateView: ");
+
+                FragmentTransaction fragmentTransaction = getActivity()
+                    .getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.nav_home1, new HomeFragment());
+            fragmentTransaction.commit();
+            }
+        });
         return root;
     }
 

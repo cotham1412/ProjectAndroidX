495fcd7ca55f59bb4ae7abf04a2a32ef70ced471
20211tt4780@mail.tdc.edu.vn <20211tt4780@mail.tdc.edu.vn>
2022-05-16 23:56:17 +0700

    fix things

--- ProjectAndroidX/app/src/main/java/com/example/projectandroidx/MainActivity.java
+++ ProjectAndroidX/app/src/main/java/com/example/projectandroidx/MainActivity.java
@@ -22,6 +22,20 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
+//     cac buoc tao 1 muc trong nav :
+//        b1: vao` muc. ui -> tao. package moi' dat ten theo chuc' nang -> tao. file Fragment va` file ViewModel
+//        b2: vao` muc. navigation -> mobile_navigation.xml -> copy 1 fragment -> doi id fragment (co' nav o dau`)
+//                  -> keo' ra 1 cho moi'
+//        b3: vao` muc. layout -> tao. 1 fragment moi' , dat ten theo chuc' nang -> trang tri' trang do'
+//        b4: vao` muc. res -> chuot phai-> new -> vector Asset -> chon. Local file -> dat ten vector theo icon
+//                  ->  den phan` path thi` lan chuot. tim` icon can` dung`,chinh ti le neu can`-> next
+//        b5: vao` MainActivity -> tai. dong` mAppBarConfiguration = new AppBarConfiguration.Builder
+//                  -> them R.id. (ten id moi' dat. o b2)
+//        b6: vao` values -> string.xml -> them string name theo chuc' nang
+//        b7: vao` menu -> activity-main -> them 1 muc vao` , doi icon
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_home1)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_detail)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
